0173720f863f7d0bf14db59df51a65522b424159
NRodrigoMunoz <munoznuneznestorrodrigo@gmail.com>
2024-10-11 23:28:19 +0000

    Arreglado el puntof y actualizado el puntoe

--- parcial1/puntoe.sh
+++ parcial1/puntoe.sh
@@ -1,3 +1,3 @@
-curl -s ifconfig.me|awk '{print "Mi IP Publica es: " $1}' > filtroavanzado.txt
-grep vagrant /etc/passwd|awk -F ':' '{print "Mi usuario es: " $1}' >> filtroavanzo.txt
-sudo cat /etc/shadow|grep vagrant|awk -F ':' '{print "El hast de mi Usuario es: " $2}' >> filtroavanzo.txt
+cat /proc/meminfo |grep MemTotal > filtrobasico.txt
+sudo dmidecode -t chassis |grep Chassis >> filtrobasico.txt
+sudo dmidecode -t chassis |grep Manufacturer >> filtrobasico.txt
--- parcial1/puntof.sh
+++ parcial1/puntof.sh
@@ -1,1 +1,3 @@
-vagrant
+curl -s ifconfig.me|awk '{print "Mi IP Publica es: " $1}' > filtroavanzado.txt
+grep vagrant /etc/passwd|awk -F ':' '{print "Mi usuario es: " $1}' >> filtroavanzo.txt
+sudo cat /etc/shadow|grep vagrant|awk -F ':' '{print "El hast de mi Usuario es: " $2}' >> filtroavanzo.txt
